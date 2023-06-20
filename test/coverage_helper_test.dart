fbede0803194787847664818dfcd9ff0f882932c
Seungbin Oh <sboh1214@gmail.com>
2023-06-20 16:21:30 +0900

    add widget tests

---
+++ test/coverage_helper_test.dart
@@ -0,0 +1,22 @@
+// Helper file to make coverage work for all dart files.
+
+// ignore_for_file: unused_import
+import 'package:hobby_hive/providers/user_provider.dart';
+import 'package:hobby_hive/firebase_options.dart';
+import 'package:hobby_hive/models/user_model.dart';
+import 'package:hobby_hive/models/chatroom_model.dart';
+import 'package:hobby_hive/models/chat_model.dart';
+import 'package:hobby_hive/models/event_model.dart';
+import 'package:hobby_hive/main.dart';
+import 'package:hobby_hive/pages/main_page.dart';
+import 'package:hobby_hive/pages/chatroom.dart';
+import 'package:hobby_hive/pages/setting_page.dart';
+import 'package:hobby_hive/pages/sign_in_page.dart';
+import 'package:hobby_hive/pages/sign_up_page.dart';
+import 'package:hobby_hive/pages/create_event_page.dart';
+import 'package:hobby_hive/pages/view_profile_page.dart';
+import 'package:hobby_hive/pages/bottom_navigation_page.dart';
+import 'package:hobby_hive/pages/fix_profile_page.dart';
+import 'package:hobby_hive/widgets/interest_chip.dart';
+import 'package:hobby_hive/widgets/loading_indicator.dart';
+void main(){}
